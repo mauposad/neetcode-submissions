ff18d4ee8a5c875d4f6e56b812ab3178a7cf014a
Mauricio Posadas <112008257+mauposad@users.noreply.github.com>
2026-07-10 20:28:23 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list) #hashmap of solutions
+
+        for s in strs: # for loop to traverse strs
+            count = [0] * 26 #array for key value for hash map
+            for c in s: #traversing each letter in word for key
+                count[ord(c)-ord("a")]+=1 #calculating key by using ascii values 
+            result[tuple(count)].append(s)# setting value of key to the instance of word
+        return list(result.values()) # win condition
+            # the result.values just returns dictionary. must be wrapped in list
