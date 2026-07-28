f14f36401e8b172c2c4fb65fbb82f1cf502ad808
Mauricio Posadas <112008257+mauposad@users.noreply.github.com>
2026-07-27 19:16:52 -0700

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int: # my own solution, not the best time complexity
+        l, r  = 0, 1
+        profit = 0
+        
+        while r < len(prices):
+            trade_value = prices[r]-prices[l]
+            if trade_value > 0:
+                profit = max(profit, trade_value)
+                r+=1
+            elif trade_value < 0: 
+                l=r
+                r+=1
+            else:
+                r+=1
+        return profit
